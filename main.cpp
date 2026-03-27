f8468a9125236cb6e47c78d94791f35fa7c47907
caiomiguelbecker <caiomiguelbecker@gmail.com>
2026-03-27 20:18:14 -0300

    soma de números

--- main.cpp
+++ main.cpp
@@ -1,20 +1,11 @@
 #include<iostream>
-#include<iomanip>
 
 int main(){
 
-int a = 8;
-//int número inteiro.
-float b = 9.6;
-//float número de ponto flutuante ocupa 4bytes.
-double c = 4.6276548976;
-//double ocupa 8bytes.
+std::cout<<8+2<<std::endl;
+std::cout<<10-4<<std::endl;
+std::cout<<4*3<<std::endl;
+std::cout<<8/2<<std::endl;
 
- std::cout<<a<<std::endl;
- std::cout<<b<<std::endl;
- std::cout<<c<<std::endl;
-
-std::cout<<std::fixed<<std::setprecision(10)<<c<<std::endl;
-//configura o número de caracteres do double, pois ele não pasará de 7 caracteres sem setprecision.
      return 0;
 }
