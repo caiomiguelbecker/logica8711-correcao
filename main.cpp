2b478060eb2780f19537cbdcc53d1bdb7a8f1165
caiomiguelbecker <caiomiguelbecker@gmail.com>
2026-03-27 20:08:30 -0300

    setprecision

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<iomanip>
 
 int main(){
 
@@ -6,13 +7,14 @@ int a = 8;
 //int número inteiro.
 float b = 9.6;
 //float número de ponto flutuante ocupa 4bytes.
-double c = 4.6;
-//double ocupa 8bytes.,.
-
+double c = 4.6276548976;
+//double ocupa 8bytes.
 
  std::cout<<a<<std::endl;
  std::cout<<b<<std::endl;
  std::cout<<c<<std::endl;
 
+std::cout<<std::fixed<<std::setprecision(10)<<c<<std::endl;
+//configura o número de caracteres do double, pois ele não pasará de 7 caracteres sem setprecision.
      return 0;
 }
