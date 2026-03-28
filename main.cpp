15cec8e1de54442a75bbe41959cabbe35953a493
caiomiguelbecker <caiomiguelbecker@gmail.com>
2026-03-27 21:32:28 -0300

    std::cin>>nome;

--- main.cpp
+++ main.cpp
@@ -3,13 +3,13 @@
 
 int main(){
 
-int idade = 18;
-float altura = 1.83;
-std::string nome = "Caio";
+std::string nome;
 
-std::cout<<" meu nome e "<<nome<< "."<<std::endl;
-std::cout<<" minha altura e "<<altura<< "m."<<std::endl;
-std::cout<<" minha idade e "<<idade<<" anos."<<std::endl;
+std::cout<<"qual seu nome: "<<std::endl;
+std::cin>>nome;
+
+std::cout<<"bem-vindo(a) "<<nome<<"!!"<<std::endl;
+
 
 
      return 0;
