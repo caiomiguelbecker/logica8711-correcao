0b728c23002461f667d3a8659628d027913fb5fc
caiomiguelbecker <caiomiguelbecker@gmail.com>
2026-03-27 21:59:41 -0300

    std::cin / altura, nome e idade

--- main.cpp
+++ main.cpp
@@ -4,12 +4,21 @@
 int main(){
 
 std::string nome;
+int idade;
+float altura;
 
 std::cout<<"qual seu nome: "<<std::endl;
 std::cin>>nome;
+std::cout<<"qual sua idade: "<<std::endl;
+std::cin>>idade;
+std::cout<<"qual sua altura: "<<std::endl;
+std::cin>>altura;
 
-std::cout<<"bem-vindo(a) "<<nome<<"!!"<<std::endl;
 
+std::cout<<"welcome, mr. "<<nome<<" !!"<<std::endl;
+std::cout<<"Sua idade e altura sao: "<<std::endl;
+std::cout<<idade<<" anos."<<std::endl;
+std::cout<<altura<<" m."<<std::endl;
 
 
      return 0;
