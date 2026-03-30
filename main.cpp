8d548eb9b504bfccdd8cdc46f1e7d4449bdcc1c2
caiomiguelbecker <caiomiguelbecker@gmail.com>
2026-03-30 20:27:15 -0300

    comparação de variaveis com binarios

--- main.cpp
+++ main.cpp
@@ -1,14 +1,23 @@
 #include<iostream>
-#include<string>
 
 int main(){
 
-     std::string diadasemana;
+     int a;
+     int b;
+     //int a,b;
 
-     std::cout<<"Que dia é hoje: "<<std::endl;
-     std::cin>>diadasemana;
+     std::cout<<"Digite um valor para A: "<<std::endl;
+     std::cin>>a;
 
-     std::cout<<"Hoje é "<<diadasemana<<"!!"<<std::endl;
+     std::cout<<"Digite um valor para B: "<<std::endl;
+     std::cin>>b;
+
+     std::cout<<(a > b )<<std::endl;
+     //Linguagem de máquina: true 1 ----- false 0, binário
+
+     std::cout<<(a >= b)<<std::endl;
+     std::cout<<(a != b)<<std::endl;
+     
 
 
 
