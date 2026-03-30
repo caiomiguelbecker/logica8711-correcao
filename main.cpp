43b2c48e43fbe532e6fd800ba91e870fe079933e
caiomiguelbecker <caiomiguelbecker@gmail.com>
2026-03-30 20:04:27 -0300

    variavel dia da semana

--- main.cpp
+++ main.cpp
@@ -3,22 +3,13 @@
 
 int main(){
 
-std::string nome;
-int idade;
-float altura;
+     std::string diadasemana;
 
-std::cout<<"qual seu nome: "<<std::endl;
-std::cin>>nome;
-std::cout<<"qual sua idade: "<<std::endl;
-std::cin>>idade;
-std::cout<<"qual sua altura: "<<std::endl;
-std::cin>>altura;
+     std::cout<<"Que dia é hoje: "<<std::endl;
+     std::cin>>diadasemana;
 
+     std::cout<<"Hoje é "<<diadasemana<<"!!"<<std::endl;
 
-std::cout<<"welcome, mr. "<<nome<<" !!"<<std::endl;
-std::cout<<"Sua idade e altura sao: "<<std::endl;
-std::cout<<idade<<" anos."<<std::endl;
-std::cout<<altura<<" m."<<std::endl;
 
 
      return 0;
