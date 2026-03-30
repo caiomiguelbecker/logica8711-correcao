acd68e4a7bff67030503544c0af6679e93714ddc
caiomiguelbecker <caiomiguelbecker@gmail.com>
2026-03-30 20:36:01 -0300

    atualização comparação e adição de variaveis

--- main.cpp
+++ main.cpp
@@ -12,12 +12,19 @@ int main(){
      std::cout<<"Digite um valor para B: "<<std::endl;
      std::cin>>b;
 
+     std::cout<<"A comparacao das variaveis: "<<std::endl;
+
      std::cout<<(a > b )<<std::endl;
      //Linguagem de máquina: true 1 ----- false 0, binário
 
      std::cout<<(a >= b)<<std::endl;
+     // >= maior ou igual 
+
      std::cout<<(a != b)<<std::endl;
-     
+     // != se o numero é diferente do outro
+
+     std::cout<<(a == b)<<std::endl;
+     // == para peguntar se é igual
 
 
 
