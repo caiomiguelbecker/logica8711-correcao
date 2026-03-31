b7cbc7d2421e9a3b526a2def2bf92739be5f1beb
caiomiguelbecker <caiomiguelbecker@gmail.com>
2026-03-30 21:15:27 -0300

    soma com progrma

--- main.cpp
+++ main.cpp
@@ -2,29 +2,14 @@
 
 int main(){
 
-     int a;
-     int b;
-     //int a,b;
+    int soma1 = 100 + 50;
+    int soma2 = soma1 + 50;
+    int soma3 = soma2 + 100;
 
-     std::cout<<"Digite um valor para A: "<<std::endl;
-     std::cin>>a;
+    std::cout<<soma2<<std::endl;
+    std::cout<<soma3<<std::endl;
 
-     std::cout<<"Digite um valor para B: "<<std::endl;
-     std::cin>>b;
 
-     std::cout<<"A comparacao das variaveis: "<<std::endl;
-
-     std::cout<<(a > b )<<std::endl;
-     //Linguagem de máquina: true 1 ----- false 0, binário
-
-     std::cout<<(a >= b)<<std::endl;
-     // >= maior ou igual 
-
-     std::cout<<(a != b)<<std::endl;
-     // != se o numero é diferente do outro
-
-     std::cout<<(a == b)<<std::endl;
-     // == para peguntar se é igual
 
 
 
