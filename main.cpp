057bf57f1b51f962df1073476d3252bc46a9daca
caiomiguelbecker <caiomiguelbecker@gmail.com>
2026-04-01 19:48:47 -0300

    variavel auto

--- main.cpp
+++ main.cpp
@@ -2,10 +2,19 @@
 
 int main(){
 
-     char letra = 'E';
+    auto num1 = 10;
+    auto num2 = 15.80f;
+    auto letra = 'e';
+    auto texto = "caio";
 
-     std::cout<<letra<<std::endl;
-     
+
+    std::cout<<num1<<std::endl;
+    std::cout<<num2<<std::endl;
+    std::cout<<letra<<std::endl;
+    std::cout<<texto<<std::endl;
+    
+
+ 
 
 
      return 0;
