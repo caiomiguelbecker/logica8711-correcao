023e9bd2ea5e0f7a621aad115052682f10c5610d
caiomiguelbecker <caiomiguelbecker@gmail.com>
2026-04-13 21:14:27 -0300

    implementação de juros e desconto

--- main.cpp
+++ main.cpp
@@ -3,31 +3,39 @@
 
 int main(){
 
-  int idade;
+    int opcaoPagamento;
+    int qtdParafuso;
+    float valorParafuso = 1.40;
+    float valorTotalParafuso;
+    float desconto;
+    float juros;
 
-  std::cout<<"Digite sua idade"<<std::endl;
-  std::cin>>idade;
+    std::cout<<"Digite a quantidade de para fusos desejada: "<<std::endl;
+    std::cin>>qtdParafuso;
 
-  if(idade < 0){
-    std::cout<<"Idade invalida!!"<<std::endl;
-  }
-  else if (idade <=2){
-    std::cout<<"Voce e um bebe!"<<std::endl;
-  }
-  else if(idade <= 8){
-    std::cout<<"voce e uma crianca!"<<std::endl;
-  }
-  else if(idade <= 17){
-    std::cout<<"Voce e um(a) adolecente!"<<std::endl;
-  }
-  else if(idade <= 60){
-    std::cout<<"Voce e adulto(a)!"<<std::endl;
-  }
-    else{
-      std::cout<<"Voce e um(a) senior!"<<std::endl;
-  }
+    valorTotalParafuso = qtdParafuso * valorParafuso;
 
+    std::cout<<"qual forma de pagamento? "<<std::endl;
+    std::cout<<"--- 1 - pix"<<std::endl;
+    std::cout<<"--- 2 - dinheiro"<<std::endl;
+    std::cout<<"--- 3 - cartao"<<std::endl;
+    std::cin>>opcaoPagamento;
+
+    if(opcaoPagamento == 1){
+      std::cout<<"Sua comprar ficou no valor total de R$: "<<valorTotalParafuso<<std::endl;
+    }
+    else if(opcaoPagamento == 2){
+      desconto = valorTotalParafuso * 0.90;
+      std::cout<<"sua compra ficou no valor total de R$: "<<desconto<<std::endl;
+    }
+    else if(opcaoPagamento == 3){
+      juros = valorTotalParafuso * 1.10;
+      std::cout<<"Sua compra ficou no valor total de R$: "<<juros<<std::endl;
+    }
+    else{ 
+      std::cout<<"Opcao de pagamento inexistente!!"<<std::endl;
+    }
 
     return 0;
-    }
+}
 
