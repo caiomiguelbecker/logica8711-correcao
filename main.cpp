9fdcb8047975e3f124097bdca7ab11349bfa43e4
caiomiguelbecker <caiomiguelbecker@gmail.com>
2026-04-15 19:46:40 -0300

    loop com tempo

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include<iostream>
-#include<string>
-
+#include<thread> //para lidard com o tempo de execução
+#include<chrono> //para definir unidades de tempo(ms, segundo)
 int main(){
 
     for(int i = 10; i >= 0; i--){
       std::cout<<"A bomba ira explodir em.."<<i<<std::endl;
+       std::this_thread::sleep_for(std::chrono::milliseconds(500));
+       //igual controlado por segundos = seconds -> milliseconds
     }
+
+   
 
 
  
