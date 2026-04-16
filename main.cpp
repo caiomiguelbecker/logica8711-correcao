f3fac08377e234cb9b9d1f9f332e871c23d423ab
caiomiguelbecker <caiomiguelbecker@gmail.com>
2026-04-15 21:58:07 -0300

    array e for

--- main.cpp
+++ main.cpp
@@ -3,13 +3,12 @@
 int main()
 {
 
-    int mynumbers[5] = {10, 20, 30, 40, 50};
-    // for(int num : mynumbers){
-    //     std::cout<<num<<std::endl;   
-    for(int i = 0; i < 5; ++i){
-        std::cout<<mynumbers[i]<<std::endl;
-    }
+    int mynumbers[9] = {10, 20, 30, 40, 50, 60, 70, 80, 90};
+   
+    for(int num : mynumbers){
 
+      std::cout<<num<<std::endl;
+    }
 
   return 0;
 }
