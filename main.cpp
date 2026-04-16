0a65134dff1f756bf23370195464bf21efc6434c
caiomiguelbecker <caiomiguelbecker@gmail.com>
2026-04-15 21:48:53 -0300

    variações de array com for

--- main.cpp
+++ main.cpp
@@ -4,8 +4,10 @@ int main()
 {
 
     int mynumbers[5] = {10, 20, 30, 40, 50};
-    for(int num : mynumbers){
-        std::cout<<num<<std::endl;   
+    // for(int num : mynumbers){
+    //     std::cout<<num<<std::endl;   
+    for(int i = 0; i < 5; ++i){
+        std::cout<<mynumbers[i]<<std::endl;
     }
 
 
