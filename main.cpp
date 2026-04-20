dc88f35aa4c1949a6d43bb2bb35ecd41e32e7c6b
caiomiguelbecker <caiomiguelbecker@gmail.com>
2026-04-20 20:14:00 -0300

    divisão de um sizeof para saber quanto ocupa de memoria

--- main.cpp
+++ main.cpp
@@ -3,12 +3,12 @@
 
 int main(){
 
+   
     std::string carros[5] = {"volvo", "bmw", "ford", "chevrolet", "fiat"};
 
-    for(int i = 0; i < 5; i++){ 
-      std::cout<<carros[i]<<std::endl;
-    }
+    int tamanho = sizeof(carros) / sizeof(carros[0]);
 
+    std::cout<<tamanho<<std::endl;
 
 
   return 0;
