18d69b41c720b9c712409910b81edd796389137d
caiomiguelbecker <caiomiguelbecker@gmail.com>
2026-04-20 19:42:02 -0300

    array com string

--- main.cpp
+++ main.cpp
@@ -1,44 +1,13 @@
 #include <iostream>
 #include<string>
 
-int main()
-{
-  std::cout << "\nHello user\n"<< std::endl;
+int main(){
 
-  std::cout << "You this node my system. " << std::endl;
-  std::cout << "The objetive test my knowledge.\n\n " << std::endl;
+    std::string nomes[3] = {"joao", "paula", "murilo"};
+    int idades[3] = {27, 31, 20};
 
-  std:: string name;
-  int age;
-  float height;
-
-  std::cout<<"what's your name? "<<std::endl;
-  std::cin>>name;
-  std::cout<<"How old are you? "<<std::endl;
-  std::cin>>age;
-  std::cout<<"How tall are you? "<<std::endl;
-  std::cin>>height;
-
-  std::cout<<"\n\nYour name and: "<<name<<std::endl;
-  std::cout<<"Your age and: "<<age<<std::endl;
-  std::cout<<"Its height and: "<<height<<std::endl;
-
-  char response;
-
-  std::cout<<"Do you want to register to join the application? (y/n)"<<std::endl;
-  std::cin>>response;
-
-  if(response == 'y' || response == 'Y'){
-    std::cout<<"welcome to the system!!"<<std::endl;
-    
-  }
-  else if(response == 'n' || response == 'N'){
-    std::cout<<"Thank you for using the system."<<std::endl;
-  }
-  else {
-    std::cout<<"Invalid option."<<std::endl;
-  }
-  
+    std::cout<<nomes[1]<<" tem "<<idades[1]<<" anos!"<<std::endl;
+    std::cout<<nomes[2]<<" tem "<<idades[2]<<" anos!"<<std::endl;
 
 
   return 0;
