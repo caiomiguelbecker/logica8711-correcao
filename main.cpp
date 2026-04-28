dcf060abf9d01918b5bd6ea65208338c051c58f8
caiomiguelbecker <caiomiguelbecker@gmail.com>
2026-04-28 20:15:48 -0300

    função void

--- main.cpp
+++ main.cpp
@@ -1,17 +1,14 @@
 #include<iostream>
-#include<vector>
+
+void myFunction(){
+    std::cout<< "hello word"<<std::endl;
+}
 
 int main(){
 
-    int myNumbers[5] = {1, 2, 3, 4, 5};
+    myFunction();
 
-    // for(int i = 0; i < 5; i++){
-    //     std::cout<<myNumbers[i]<<std::endl;
-    // }
 
-    for(int num : myNumbers){
-        std::cout<<num<<std::endl;
-    }
 
 
      return 0;
