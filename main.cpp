84249ac81caddd4aa0e0224541a10b4b5cee286a
caiomiguelbecker <caiomiguelbecker@gmail.com>
2026-04-28 21:12:38 -0300

    void (string e idade)

--- main.cpp
+++ main.cpp
@@ -1,14 +1,13 @@
 #include<iostream>
+#include<string>
 
-void minhaFuncao(std::string nome){
-    std::cout<<nome<<std::endl;
+void minhaFuncao(std::string nome, int idade){
+    std::cout<<"Meu nome e "<<nome<<" e tenho "<<idade<<" anos."<<std::endl;
 }
 
 int main(){
 
-    minhaFuncao("caio");
-    minhaFuncao("nome");
-
+    minhaFuncao("caio", 18);
 
      return 0;
 }
