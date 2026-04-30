863d542aad3d5968673eee18935370a476234abd
caiomiguelbecker <caiomiguelbecker@gmail.com>
2026-04-30 20:25:27 -0300

    cambio

--- main.cpp
+++ main.cpp
@@ -3,29 +3,29 @@
 
 int main(){
 
-    int choice;
-    float temp, converted;
+    //brlAmount: Valor em reais, | exchengRate: taxa de cambio | usdAmount: valor em dolares;
+    double brlAmount, exechangeRate, usdAmount;
+    int choice; //choice: escolha;
 
-    std::cout<<"----- Temperatura converter ------"<<std::endl;
-    std::cout<<"1: Celsius to Fahrenheit"<<std::endl;
-    std::cout<<"2: Fahreinheit to Celsius"<<std::endl;
-    std::cout<<"Choose on option (1 or 2): "<<std::endl;
-    std::cin>>choice;
+    std::cout<<" ----- Currency converter: BRL to USD ------"<<std::endl;
 
-    if(choice == 1){
-        std::cout<<"Enter temperatura in Celsius: "<<std::endl;
-        std::cin>>temp;
-        converted = (temp * 1.8) + 32;
-        std::cout<<temp<<"C is "<<converted<<" F."<<std::endl;
-    }else if(choice == 2){
-        std::cout<<"Enter temperature in Fahrenheit: "<<std::endl;
-        std::cin>>temp;
-        converted = (temp - 32) / 1.8;
-        std::cout<<temp<<"F is "<<converted<<" C."<<std::endl;
+    //entrada da taxa de cambio (quanto vale 1 dolar hoje)
+    std::cout<<"Enter the current exchange rate (How much is 1 dollar in Reais?): "<<std::endl;
+    std::cin>>exechangeRate;
+
+    //Entrada de quantia em reais que a pessoa deseja converter
+    std::cout<<"Enter the amout in BRL(R$): "<<std::endl;
+    std::cin>>brlAmount;
+
+    //verificar logica: garante que o valor informado seja positivo
+    if(brlAmount > 0){
+        //calculo: divide o montante em reais pela taxa de cambio
+        usdAmount = brlAmount / exechangeRate;
+        std::cout<<"You will have $"<<usdAmount<<" US dollars. "<<std::endl;
     }else{
-        std::cout<<"Invalid option! Please restart."<<std::endl;
+        //mensagem de erro caso o valor seja zero ou negativo
+        std::cout<<"Error: Please enter a valid amount greater than zero."<<std::endl;
     }
-
 
 
 
