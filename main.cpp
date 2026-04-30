84b4520bac5c5888f46401f438d08a5e8f690095
caiomiguelbecker <caiomiguelbecker@gmail.com>
2026-04-30 20:59:15 -0300

    ATIVIDADE DE PORCENTAGEM IMPAR PAR

--- main.cpp
+++ main.cpp
@@ -3,29 +3,22 @@
 
 int main(){
 
-    //brlAmount: Valor em reais, | exchengRate: taxa de cambio | usdAmount: valor em dolares;
-    double brlAmount, exechangeRate, usdAmount;
-    int choice; //choice: escolha;
+    int num;
 
-    std::cout<<" ----- Currency converter: BRL to USD ------"<<std::endl;
+    std::cout<<"Digite um valor para num: "<<std::endl;
+    std::cin>>num;
 
-    //entrada da taxa de cambio (quanto vale 1 dolar hoje)
-    std::cout<<"Enter the current exchange rate (How much is 1 dollar in Reais?): "<<std::endl;
-    std::cin>>exechangeRate;
 
-    //Entrada de quantia em reais que a pessoa deseja converter
-    std::cout<<"Enter the amout in BRL(R$): "<<std::endl;
-    std::cin>>brlAmount;
+    if(num % 2 == 0){
+        std::cout<<num<<"O numero digitado e par "<<std::endl;
 
-    //verificar logica: garante que o valor informado seja positivo
-    if(brlAmount > 0){
-        //calculo: divide o montante em reais pela taxa de cambio
-        usdAmount = brlAmount / exechangeRate;
-        std::cout<<"You will have $"<<usdAmount<<" US dollars. "<<std::endl;
     }else{
-        //mensagem de erro caso o valor seja zero ou negativo
-        std::cout<<"Error: Please enter a valid amount greater than zero."<<std::endl;
+        std::cout<<num<<"O numero digitado e impar "<<std::endl;
+   
     }
+
+
+
 
 
 
