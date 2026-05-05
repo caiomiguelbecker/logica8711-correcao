61c59dcf5e170c8551041508a4c3a09cda5d33de
caiomiguelbecker <caiomiguelbecker@gmail.com>
2026-05-05 19:54:50 -0300

    getline funçao de espaço

--- main.cpp
+++ main.cpp
@@ -3,18 +3,13 @@
 
 int main(){
 
-    int num;
+    
+    std::string nomeCompleto;
 
-    int totalSeconds;
+    std::cout<<"Digite o nome completo do aluno: "<<std::endl;
+    std::getline(std::cin, nomeCompleto);
 
-    std::cout<<"Enter total seconds: "<<std::endl;
-    std::cin>>totalSeconds;
-
-    int minutes = totalSeconds / 60;
-    int seconds = totalSeconds % 60;
-
-    std::cout<<"Time: "<<minutes<<" minutes and "<<seconds<< " seconds."<<std::endl;
-
+    std::cout<<nomeCompleto<<std::endl;
 
 
 
