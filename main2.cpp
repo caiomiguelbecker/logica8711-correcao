0837319aa30a7e022687f23711d1bcabce66b564
caiomiguelbecker <caiomiguelbecker@gmail.com>
2026-05-12 19:23:33 -0300

    calculadora simples

---
+++ main2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+#include<windows.h>
+#include<iomanip>
+
+int main(){
+
+//menu inicial;
+
+std::cout<<"================ Bem vindo ao Senac NH ================="<<std::endl;
+
+
+
+
+
+    return 0
+};
---
+++ main3.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+int main(){
+
+    int num1, num2, soma, subt, mult;
+    float div;
+
+    std::cout<<"Digite aqui o valor para o numero 1: "<<std::endl;
+    std::cin>>num1;
+
+    std::cout<<"Digite aqui o valor para o numero 2: "<<std::endl;
+    std::cin>>num2;
+
+    soma = num1 + num2;
+    subt = num1 - num2;
+    mult = num1 * num2;
+    div = num1 / num2;
+
+    std::cout<<"O resultado da soma e: "<<soma<<std::endl;
+    std::cout<<"O resultado da subtracao e: "<<subt<<std::endl;
+    std::cout<<"O resultado da multiplicaçao e: "<<mult<<std::endl;
+    std::cout<<"O resultado da divisao e: "<<div<<std::endl;
+
+
+
+    return 0;
+}
