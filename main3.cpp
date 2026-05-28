d9bbbd301b22fd7fe10978939f52c2d751e8ccd1
caiomiguelbecker <caiomiguelbecker@gmail.com>
2026-05-28 19:43:58 -0300

    fibonacci

--- main3.cpp
+++ main3.cpp
@@ -1,29 +1,20 @@
 #include<iostream>
-#include<tuple>
-#include<string>
 
 int main(){
 
-    std::string nome;
-    int idade;
-    double altura;
+    int fibonacci[10];
 
-    std::cout<<"Digite seu nome: "<<std::endl;
-    std::getline(std::cin, nome);
+    fibonacci[0] = 1;
+    fibonacci[1] = 1;
 
-    std::cout<<"Digite a sua idade: "<<std::endl;
-    std::cin>>idade;
+    for(int i = 2; i < 10; i++){
+        fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
+    }
 
-    std::cout<<"Digite a sua altura: "<<std::endl;
-    std::cin>>altura;
-
-    std::tuple<std::string, int, double > pessoa =  std::make_tuple(nome, idade, altura);
-
-    std::cout<<"\nNome: "<<std::get<0>(pessoa)<<std::endl;
-    std::cout<<"Idade: "<<std::get<1>(pessoa)<<" anos."<<std::endl;
-    std::cout<<"Altura: "<<std::get<2>(pessoa)<<"m"<<std::endl;
+    for(int i = 0; i < 10; i++){
+        std::cout<<fibonacci[i]<<"\n";
+    }
 
 
-   
     return 0;
 }
