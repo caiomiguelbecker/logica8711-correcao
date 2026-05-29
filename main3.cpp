c2b05cfcb47f07a121de9ebcc7739d52261c9fcd
caiomiguelbecker <caiomiguelbecker@gmail.com>
2026-05-28 21:20:39 -0300

    recursão

--- main3.cpp
+++ main3.cpp
@@ -1,27 +1,26 @@
-#include<iostream>
+#include<iostream> 
 
-    int fibonacci(int n){
-        std::cout<<"\nCalculando fib("<< n<<")\n"<<std::endl;
+    int contarDigitos(int n){
+        if(n == 0) return 0;
 
-        if(n == 0){
-            std::cout<<"Fib(0) = 0 (PARADA)"<<std::endl;
-            return 0;
-        }
-        if(n == 1){
-            std::cout<<"Fib(1) = 1 (PARADA!)"<<std::endl;
-            return 1;
-        }
-
-        std::cout<<"Fib("<<n<<") = Fib("<<(n-1)<<") + fib ("<<(n-2)<<")\n";
-        int resultado = fibonacci(n -1) + fibonacci(n -2);
-        std::cout<<"Fib(" << n << ") = "<<resultado<< "\n";
-        return resultado;
+        return 1 + contarDigitos (n / 10);
     }
 
 
 int main(){
 
-    fibonacci(4);
+    int numeroUsuario;
+
+    std::cout<<"Digite um número inteiro: "<<std::endl;
+    std::cin>>numeroUsuario;
+
+    if(numeroUsuario == 0){
+        std::cout<<"O número 0 tem 1 dígito.\n";
+    }else{
+        std::cout<<"O número "<<numeroUsuario<<" tem "<<contarDigitos(numeroUsuario)<<" dígitos."<<std::endl;
+    }
+
     
+
     return 0;
 }
