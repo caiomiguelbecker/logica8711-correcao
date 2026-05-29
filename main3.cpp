db76d170d325db535602505b84ac713181fc4eff
caiomiguelbecker <caiomiguelbecker@gmail.com>
2026-05-28 21:39:23 -0300

    recursão com tabuada

--- main3.cpp
+++ main3.cpp
@@ -1,26 +1,30 @@
 #include<iostream> 
 
-    int contarDigitos(int n){
-        if(n == 0) return 0;
+   void tabuada(int numero, int multiplicador){
+       if(multiplicador == 0){
+            return;
+       } 
 
-        return 1 + contarDigitos (n / 10);
-    }
+        tabuada(numero, multiplicador - 1);
+
+        std::cout<<numero<<" x "<<multiplicador<<" = "<<(numero * multiplicador)<<std::endl;
+
+   }
 
 
 int main(){
 
-    int numeroUsuario;
+    int numero;
+    int multiplicador;
 
-    std::cout<<"Digite um número inteiro: "<<std::endl;
-    std::cin>>numeroUsuario;
+    std::cout<<"Digite um numero: "<<std::endl;
+    std::cin>>numero;
 
-    if(numeroUsuario == 0){
-        std::cout<<"O número 0 tem 1 dígito.\n";
-    }else{
-        std::cout<<"O número "<<numeroUsuario<<" tem "<<contarDigitos(numeroUsuario)<<" dígitos."<<std::endl;
-    }
+    std::cout<<"Digite um multiplicador: "<<std::endl;
+    std::cin>>multiplicador;
 
-    
+    tabuada(numero, multiplicador);
+
 
     return 0;
 }
