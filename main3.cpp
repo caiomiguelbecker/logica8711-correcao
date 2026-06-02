c10c391a8154c252410ff4c1abe9aabdf69bbcd4
caiomiguelbecker <caiomiguelbecker@gmail.com>
2026-06-02 19:48:25 -0300

    palindromo

--- main3.cpp
+++ main3.cpp
@@ -1,21 +1,41 @@
 #include<iostream> 
+#include<string>
+#include<cctype>
 
-   int somarVetor(int vetor[], int indice){
-    if(indice == 0){
-        return vetor[0];
+bool ehPalindromo(std::string s){
+    int inicio = 0;
+    int fim = s.length() - 1;
+
+    while(inicio < fim){
+        if(s[inicio] != s[fim]){
+            return false;
+        }
+        inicio++;
+        fim--;
     }
-    
-    return vetor[indice] + somarVetor(vetor, indice - 1);
-
-   }
-
+    return true;
+}
 
 int main(){
+  
+    std::string palavra;
 
-    int numeros[5] = {10,20,30,40,50};
-
-    std::cout<<"Soma recursiva: "<<somarVetor(numeros, 4)<<std::endl;
-
-
+    std::cout<<"Digite uma palavra: "<<std::endl;
+    std::cin>>palavra;
+  
+  
+    for(int i = 0; i < palavra.length(); i++){
+            palavra[i] = std::tolower(palavra[i]);
+    }
+    
+    if(ehPalindromo(palavra)){
+        std::cout<<"É palíndromo!"<<std::endl;
+    }else{
+        std::cout<<"Não é palíndromo!"<<std::endl;
+    }
+ 
+ 
+ 
+    
     return 0;
 }
