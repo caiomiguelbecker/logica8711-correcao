aae3a80b0427724ea5cc77856a79d29e5bed9a17
caiomiguelbecker <caiomiguelbecker@gmail.com>
2026-06-17 20:07:36 -0300

    calculadora  de aposta

--- main3.cpp
+++ main3.cpp
@@ -1,43 +1,33 @@
 #include<iostream>
 #include<string>
 
-struct jogador{
-    std::string nome;
+struct Aposta{
     std::string time;
-    int gols;
+    int valor;
+    int odd;
 };
 
 int main(){
-    jogador jogadores[5];
-
-    std::cout<<"===== COPA - RANKING DE GOLEADORES ====="<<std::endl;
+    std::cout<<"===== CALCULADORA DE APOSTAS ====="<<std::endl;
     std::cout<<std::endl;
 
-    for(int i = 0; i < 5; i++){
-        std::cout<<"Jogador "<<(i + 1)<<":"<<std::endl;
-        std::cout<<"Nome: ",
-        std::cin>>jogadores[i].nome;
-        std::cout<<"Time: ";
-        std::cin>>jogadores[i].time;
-        std::cout<<"Gols: ",
-        std::cin>>jogadores[i].gols;
-        std::cout<<std::endl;
-    }
+    Aposta aposta;
 
-    std::cout<<"====== RANKING ======"<<std::endl;
+    std::cout<<"Time: ";
+    std::cin>>aposta.time;
+
+    std::cout<<"Valor da aposta (R$): ";
+    std::cin>>aposta.valor;
+
+    std::cout<<"odd (multiplicador): ";
+    std::cin>>aposta.odd;
+
+    int ganho = aposta.valor * aposta.odd;
+    int lucro = ganho - aposta.valor;
+
     std::cout<<std::endl;
-
-    for(int i = 0; i < 5 - 1; i++){
-        for(int j = 0; j < 5 -1; j++){
-            if(jogadores[i].gols < jogadores[j + 1].gols){
-                jogador temp = jogadores[j];
-                jogadores[j] = jogadores[j + 1];
-                jogadores[j + 1] = temp;
-            }
-        }
-    }
-    for(int i = 0; i < 5; i++){
-        std::cout<<(i + i)<<"º - "<<jogadores[i].nome<<" ("<<jogadores[i].time<<") - "<<jogadores[i].gols<<" gols"<<std::endl;
-    }
-    return 0;
+    std::cout<<"===== RESULTADO ====="<<std::endl;
+    std::cout<<"Você apostou R$ "<<aposta.valor<<" em "<<aposta.time<<std::endl;
+    std::cout<<"Se acertar, você ganha: R$ "<<ganho<<std::endl;
+    std::cout<<"Lucro: R$ "<<lucro<<std::endl;
 }
