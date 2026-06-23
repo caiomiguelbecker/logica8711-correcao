28a7e7145470b7334bb5efedb4a2cb4401ba96d5
caiomiguelbecker <caiomiguelbecker@gmail.com>
2026-06-23 19:24:35 -0300

    fila( primeiro que entra sai)

--- main3.cpp
+++ main3.cpp
@@ -1,24 +1,27 @@
 #include<iostream>
 
 int main(){
-    int pilha[5];
-    int topo = -1;
-    int tamanho = 5;
 
-    std::cout<<"===== PILHA ====="<<std::endl;
+    int fila[5];
+    int inicio = 0;
+    int fim = 0;
+
+    std::cout<<"========= FILA ========="<<std::endl;
     std::cout<<std::endl;
 
-    pilha[0] = 10;
-    pilha[1] = 20;
-    pilha[2] = 30;
-    topo = 2;
+    fila[0] = 100;
+    fila[1] = 200;
+    fila[2] = 300;
+    fim = 3;
 
-    std::cout<<"Elementos empilhados: 10, 20, 30"<<std::endl;
+    std::cout<<"Elementos enfileirados: 100, 200, 300"<<std::endl;
+    std::cout<<std::endl;
 
-    std::cout<<"Desempilha..."<<std::endl;
-    while(topo >= 0){
-        std::cout<<"Removida: "<<pilha[topo]<<std::endl;
-        topo--;
+    std::cout<<"Desenfileirando..."<<std::endl;
+    
+    while(inicio < fim){
+        std::cout<<"Removido: "<<fila[inicio]<<std::endl;
+        inicio++;
     }
     return 0;
 }
