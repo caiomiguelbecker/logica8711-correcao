0c76e7b78b258433b366fa1ebcfcb88c5be0b4f3
caiomiguelbecker <caiomiguelbecker@gmail.com>
2026-06-23 21:47:34 -0300

    exercicio 02 (array)

--- main.cpp
+++ main.cpp
@@ -2,13 +2,22 @@
 
 int main(){
 
-    int numeros[] = {10, 20, 30, 40, 50};
+    int soma;
+    int numeros[5] = {1, 2, 3, 4, 5};
 
     std::cout<<"Números: "<<std::endl;
 
     for(int i = 0; i < 5; i++){
         std::cout<<numeros[i]<<std::endl;
+    
+    soma += numeros[i];
+
     }
+
+    std::cout<<std::endl;
+
+    std::cout<<"Soma dos mesmos números: "<<soma<<std::endl;
+    
     
 
      return 0;
