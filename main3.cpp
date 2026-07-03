7ba1c201a5884b866251388dbb5c91cddee72a01
caiomiguelbecker <caiomiguelbecker@gmail.com>
2026-07-02 21:32:19 -0300

    limpar vector

--- main3.cpp
+++ main3.cpp
@@ -3,28 +3,21 @@
 
 int main(){
 
-   std::vector<int>numeros;
+    std::vector<int>numeros;
 
-   std::cout<<"===== Remover elemento específico ====="<<std::endl;
+    std::cout<<"===== limpar vector ====="<<std::endl;
 
-   numeros.push_back(10);
-   numeros.push_back(20);
-   numeros.push_back(30);
-   numeros.push_back(40);
+    numeros.push_back(10);
+    numeros.push_back(20);
+    numeros.push_back(30);
 
-   std::cout<<"Antes: ";
-   for(int i = 0; i < numeros.size(); i++){
-    std::cout<<numeros[i]<<" ";
-   }
+    std::cout<<"Tamanho antes: "<<numeros.size()<<std::endl;
 
-    std::cout<<std::endl;
+    numeros.clear();
 
-    numeros.erase(numeros.begin() + 1);
-
-    std::cout<<"Depois: ";
-    for(int i = 0; i < numeros.size(); i++){
-        std::cout<<numeros[i]<<" ";
-    }
+    std::cout<<"Tamanho depois: "<<numeros.size()<<std::endl;
+    std::cout<<"Vector vazio? "<<(numeros.empty() ? "sim" : "Não")<<std::endl;
    
+      
     return 0;
 }
