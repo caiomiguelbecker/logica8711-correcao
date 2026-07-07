2910dd4ac48724c5d2d53b208a4c8baf4ccece11
caiomiguelbecker <caiomiguelbecker@gmail.com>
2026-07-07 19:28:44 -0300

    soma de números de um vector

--- main3.cpp
+++ main3.cpp
@@ -5,30 +5,20 @@ int main(){
 
     std::vector<int>numeros;
 
-    std::cout<<"===== Maior e menor ====="<<std::endl;
-
-    numeros.push_back(15);
-    numeros.push_back(8);
-    numeros.push_back(25);
-    numeros.push_back(3);
-    numeros.push_back(42);
-
-    std::cout<<"Vector: ";
-    for(int i = 0; i < numeros.size(); i++){
-        std::cout<<numeros[i]<<" ";
-    }
-    std::cout<<std::endl;
-
-    int maior = numeros[0];
-    int menor = numeros[0];
-
-    for(int i = 0; i < numeros.size(); i++){
-        if(numeros[i] > maior){
-            maior = numeros[i];
-        }
-    }
-    std::cout<<"Maior: "<<maior<<std::endl;
-    std::cout<<"Menor: "<<menor<<std::endl;
+    std::cout<<"\n==== Soma dos numeros de um Vector ===="<<std::endl;
+    
+    numeros.push_back(10);
+    numeros.push_back(20);
+    numeros.push_back(30);
+    numeros.push_back(40);
       
+    int soma;
+ 
+    for(int i = 0; i < 4; i++){
+        std::cout<<"["<<numeros[i]<<"], ";
+        soma += numeros[i];
+    }
+    std::cout<<"\nA soma dos numeros e: "<<soma<<std::endl;
+
     return 0;
 }
