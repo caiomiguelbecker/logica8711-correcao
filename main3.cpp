798e85e76af224cbbec6eb3fe21495662558f372
caiomiguelbecker <caiomiguelbecker@gmail.com>
2026-07-07 19:38:49 -0300

    contadoe de ocorrencias

--- main3.cpp
+++ main3.cpp
@@ -5,20 +5,29 @@ int main(){
 
     std::vector<int>numeros;
 
-    std::cout<<"\n==== Soma dos numeros de um Vector ===="<<std::endl;
+    std::cout<<"Contar ocorrências: "<<std::endl;
     
+    numeros.push_back(5);
     numeros.push_back(10);
-    numeros.push_back(20);
-    numeros.push_back(30);
-    numeros.push_back(40);
-      
-    int soma;
- 
-    for(int i = 0; i < 4; i++){
-        std::cout<<"["<<numeros[i]<<"], ";
-        soma += numeros[i];
-    }
-    std::cout<<"\nA soma dos numeros e: "<<soma<<std::endl;
+    numeros.push_back(5);
+    numeros.push_back(15);
+    numeros.push_back(15);
+    numeros.push_back(5);
+    numeros.push_back(15);
 
+    std::cout<<"Vector: ";
+    for(int i = 0; i < numeros.size(); i++){
+        std::cout<<numeros[i]<<" ";
+    }
+
+    int contador = 0;
+    for(int i = 0; i <numeros.size(); i++){
+        if(numeros[i] == 5){
+            contador++;
+        }
+    }
+
+    std::cout<<"\nO número 5 apareceu: "<<contador<<" vezes!"<<std::endl;
+    
     return 0;
 }
